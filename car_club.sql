02a2d3860638828221c6a2dfe585d83d72e3ab16
DESKTOP-J26UKE9\zcsd <ah.yu@wisetimes.cn>
2021-02-22 17:52:40 +0800

    feat：sql数据库

--- car_club.sql
+++ car_club.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 03/02/2021 18:30:28
+ Date: 22/02/2021 17:45:06
 */
 
 SET NAMES utf8mb4;
@@ -22,35 +22,35 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `article_comment`;
 CREATE TABLE `article_comment`  (
-  `id` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `content_id` bigint(10) NULL DEFAULT NULL COMMENT '文章id',
+  `id` bigint(20) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+  `content_id` bigint(20) NULL DEFAULT NULL COMMENT '文章id',
   `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `create_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '内容',
-  `user_id` bigint(11) NOT NULL COMMENT '用户id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '论坛博客评论' ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for article_conetent
+-- Table structure for article_content
 -- ----------------------------
-DROP TABLE IF EXISTS `article_conetent`;
-CREATE TABLE `article_conetent`  (
-  `id` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `article_content`;
+CREATE TABLE `article_content`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '论坛帖标题',
   `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '帖子类型',
   `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
   `create_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '内容',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '论坛博客' ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for info
 -- ----------------------------
 DROP TABLE IF EXISTS `info`;
 CREATE TABLE `info`  (
-  `id` bigint(10) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标题',
   `type` int(1) NULL DEFAULT NULL COMMENT '帖子类型',
   `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
@@ -59,14 +59,28 @@ CREATE TABLE `info`  (
   `icon` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '图标',
   `url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '链接',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '公告栏信息' ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for review
+-- ----------------------------
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE `review`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `type` int(1) NULL DEFAULT NULL COMMENT '1->加入车队 2->退出车队',
+  `status` int(1) NULL DEFAULT NULL COMMENT '0->未审核 1->审核通过',
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '申请人',
+  `team_id` bigint(20) NULL DEFAULT NULL COMMENT '车队ID',
+  `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '申请时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for task
 -- ----------------------------
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task`  (
-  `id` bigint(10) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标题',
   `create_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
@@ -75,33 +89,33 @@ CREATE TABLE `task`  (
   `user_id` bigint(10) NULL DEFAULT NULL COMMENT '任务归属人',
   `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '任务内容',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '任务主表' ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for team
 -- ----------------------------
 DROP TABLE IF EXISTS `team`;
 CREATE TABLE `team`  (
-  `id` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '车队名字',
-  `captain` int(10) NULL DEFAULT NULL COMMENT '队长',
+  `captain` bigint(20) NULL DEFAULT NULL COMMENT '队长',
   `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '简介内容',
   `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
   `create_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建人',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '车队' ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of team
 -- ----------------------------
-INSERT INTO `team` VALUES (1, 'F1车队', 1, '咸鱼车队', '2021-01-25 19:42:38', '管理员');
+INSERT INTO `team` VALUES (1, 'F1车队', 1, '132131', '2021-02-18 16:36:56', '1');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` bigint(10) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
   `real_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '真实姓名',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
@@ -111,18 +125,15 @@ CREATE TABLE `user`  (
   `update_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   `award_info` blob NULL COMMENT '获奖信息',
   `specialty` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '特长',
-  `team_id` bigint(10) NULL DEFAULT NULL COMMENT '车队di',
+  `team_id` bigint(20) NULL DEFAULT NULL COMMENT '车队di',
   `super_admin` int(1) NULL DEFAULT 0 COMMENT '0->普通用户 1->超级管理员',
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `user_idx1_username`(`username`) USING BTREE COMMENT '用户名全局唯一',
-  INDEX `user_idx1_phone`(`phone`) USING BTREE COMMENT '用户手机号全局唯一'
-) ENGINE = InnoDB AUTO_INCREMENT = 86 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Compact;
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '管理员', '123456', '18888888888', 'https://www.google.com.hk/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png', '2018-04-20 07:15:18', '2021-01-27 18:09:10', NULL, '听音乐,打篮球，看书', 1, 0);
-INSERT INTO `user` VALUES (84, 'admin1', '管理员1', '123456', '111111111111', '1111', '2021-01-27 00:00:00', '2021-01-27 00:00:00', 0x31, '', NULL, 0);
-INSERT INTO `user` VALUES (85, 'admin2', '管理员1', '123456', '111111111111', '1111', '2021-02-02 15:53:23', '2021-02-02 15:53:23', 0x31, '', NULL, 0);
+INSERT INTO `user` VALUES (1, 'admin', 'admin', 'admin', '18888888888', '//gw.alicdn.com/imgextra/i3/O1CN01QEaQlr1idHCLFlMXQ_!!6000000004435-55-tps-106-19.svg', '2021-02-18 14:10:24', '2021-02-18 14:15:31', 0xE697A0, '1111', 1, 1);
+INSERT INTO `user` VALUES (2, 'admin1', 'admin1', 'admin1', '18888888887', '//gw.alicdn.com/imgextra/i3/O1CN01QEaQlr1idHCLFlMXQ_!!6000000004435-55-tps-106-19.svg', '2021-02-18 15:13:24', '2021-02-18 15:13:24', '', '1111', 1, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
